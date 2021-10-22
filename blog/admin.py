ab5ec5603698b646132f0786e5e65e5214417928
shane-os <shaneosullivansos@yahoo.co.uk>
2021-10-22 10:54:33 +0000

    Add blog app to site admin

---
+++ blog/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from blog.models import Post, Category
+
+
+class BlogAdmin (admin.ModelAdmin):
+    pass
+
+
+class CategoryAdmin (admin.ModelAdmin):
+    pass
+
+admin.site.register(Post, BlogAdmin)
+admin.site.register(Category, CategoryAdmin)
---
+++ blog/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Category(models.Model):    
+    name = models.CharField(max_length=25)
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    categories = models.ManyToManyField('Category', related_name='posts')
+    created_by = models.TextField()
+    body = models.TextField()
+    posted_date = models.DateTimeField(auto_now_add=True)
