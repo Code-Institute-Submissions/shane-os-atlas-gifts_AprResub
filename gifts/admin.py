a1e70270cfd6457293117091e9bcda855a73bf88
shane-os <shaneosullivansos@yahoo.co.uk>
2021-10-24 13:37:11 +0000

    Add models to gifts app

--- gifts/admin.py
+++ gifts/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Gift, Category
 
-# Register your models here.
+admin.site.register(Gift)
+admin.site.register(Category)
--- gifts/models.py
+++ gifts/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    show_name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Gift(models.Model):
+    category = models.ForeignKey('category', null=True, blank=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=4, decimal_places=2)
+    weight = models.DecimalField(max_digits=4, decimal_places=3, null=True, blank=True)
+    rating = models.ValueRange(start=1, end=5)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField()
+
+    def __str__(self):
+        return self.name
