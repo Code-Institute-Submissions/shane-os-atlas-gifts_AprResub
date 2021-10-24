71b57a14f41433e5a732c261a36c479fd47f3310
shane-os <shaneosullivansos@yahoo.co.uk>
2021-10-24 17:47:06 +0000

    Correct admin categories spelling

--- gifts/models.py
+++ gifts/models.py
@@ -2,8 +2,14 @@ from django.db import models
 
 
 class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
     name = models.CharField(max_length=200)
     show_name = models.CharField(max_length=200)
+
+
 
     def __str__(self):
         return self.name
