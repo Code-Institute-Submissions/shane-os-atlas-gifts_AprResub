8fcf10793727b1a2aa24811df37fba12d79e55c5
shane-os <shaneosullivansos@yahoo.co.uk>
2021-10-24 14:51:49 +0000

    Fix gift json field error

--- gifts/models.py
+++ gifts/models.py
@@ -7,19 +7,18 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
-    
+
     def get_show_name(self):
         return self.show_name
 
 
 class Gift(models.Model):
-    category = models.ForeignKey('category', null=True, blank=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=25)
     name = models.CharField(max_length=200)
     description = models.TextField()
     price = models.DecimalField(max_digits=4, decimal_places=2)
     weight = models.DecimalField(max_digits=4, decimal_places=3, null=True, blank=True)
-    rating = models.ValueRange(start=1, end=5)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField()
 
