0eb270dc84db145984cb6caefe13947d63df4b3e
shane-os <shaneosullivansos@yahoo.co.uk>
2021-10-25 19:19:20 +0000

    Create site contact form

--- contact/views.py
+++ contact/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
+from .forms import contactform
 
-def contact_form(request):
-    return render(request, 'contact.html')
+
+def contact_page(request):
+    if request.method == 'post':
+        form = contactform(request.post)
+        if form.is_valid():
+            form.save()
+    form = contactform()
+    context = {
+        'form': form
+    }
+    return render(request, 'contact/contact.html', context)
