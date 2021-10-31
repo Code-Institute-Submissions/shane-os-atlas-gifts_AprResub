8123540af28d4dcc018b9a9df68ee10619efba59
shane-os <shaneosullivansos@yahoo.co.uk>
2021-10-31 10:55:24 +0000

    Add shopping cart section to base template

--- atlas_gifts/urls.py
+++ atlas_gifts/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     path('', include('home.urls')),
     path('gifts/', include('gifts.urls')),
     path('blog/', include('blog.urls')),
+    path('cart/', include('cart.urls')),
     path('contact/', include('contact.urls'),)
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
