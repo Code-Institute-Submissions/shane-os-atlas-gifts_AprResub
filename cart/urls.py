56c4176468b54e75b7d349b52f17771717de52ff
shane-os <shaneosullivansos@yahoo.co.uk>
2021-11-02 17:59:27 +0000

    Add ability to add gifts to cart

--- cart/urls.py
+++ cart/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.display_cart, name='display_cart')
+    path('', views.display_cart, name='display_cart'),
+    path('add/<gift_sku>/', views.cart_item_add, name='cart_item_add')
 ]
--- cart/views.py
+++ cart/views.py
@@ -5,3 +5,18 @@ from django.shortcuts import render
 def display_cart(request):
 
     return render(request, 'cart/cart.html')
+
+
+def cart_item_add(request, gift_sku):
+
+    quantity = int(request.POST.get('quantity'))
+
+    cart = request.session.get('cart', {})
+
+    if gift_sku in list(cart.keys()):
+        cart[gift_sku] += quantity
+    else:
+        cart[gift_sku] = quantity
+
+    request.session['cart'] = cart
+    print(request.session['cart'])
