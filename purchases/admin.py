f447d33e82079d30c05581b557f6f51aae90e53a
shane-os <shaneosullivansos@yahoo.co.uk>
2021-11-19 14:31:48 +0000

    Create purchases app django admin

--- purchases/admin.py
+++ purchases/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Purchase, LineItem
 
-# Register your models here.
+
+class PurchaseAdmin(admin.ModelAdmin):
+    readonly_fields = ('order_number', 'date', 'total',)
+
+    fields = ('name', 'phone', 'email', 'address_line1',
+                 'address_line2', 'address_line3', 'town', 'postcode',
+                 'country', 'order_number', 'date', 'total',)
+
+    list_display = ('order_number', 'date', 'name', 'email', 'total',)
+
+admin.site.register(Purchase, PurchaseAdmin)
