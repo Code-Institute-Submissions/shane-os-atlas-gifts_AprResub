8e0d4317558945f74e274518bf2eedb7cdc06d0c
shane-os <shaneosullivansos@yahoo.co.uk>
2021-11-19 14:42:00 +0000

    Prevent unauthorised access of purchases page

--- purchases/views.py
+++ purchases/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
 # Create your views here.
 def order_payment(request):
 
+
+    cart = request.session.get('cart', {})
+    if not cart:
+        messages.error(request, "Your cart is currently empty! Please see our gift collection to see our exciting range!")
+        return redirect(reverse('gifts'))
     return render(request, 'purchases.html')
