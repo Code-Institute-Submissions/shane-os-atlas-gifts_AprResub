c7bbf4c3287d244764fc40a40a64675240012bb2
shane-os <shaneosullivansos@yahoo.co.uk>
2021-11-19 09:52:20 +0000

    Remove typo in model field

--- purchases/models.py
+++ purchases/models.py
@@ -6,7 +6,7 @@ class Purchase(models.Model):
 
     name = models.CharField(max_length=50, null=False, blank=False)
     phone = models.CharField(max_length=15, null=False, blank=False)
-    emails = models.EmailField(max_length=128, null=False, blank=False)
+    email = models.EmailField(max_length=128, null=False, blank=False)
     phone = models.CharField(max_length=15, null=False, blank=False)
     address_line1 = models.CharField(max_length=100, null=False, blank=False)
     address_line2 = models.CharField(max_length=100, null=False, blank=True)
