a28472407db787414007fecf82d7007ea7093097
shane-os <shaneosullivansos@yahoo.co.uk>
2021-11-19 17:44:01 +0000

    Add purchases form to app

---
+++ purchases/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from .models import Purchase, LineItem
+
+
+class PurchaseForm(forms.ModelForm):
+    class Meta:
+        model = Purchase
+        fields = ('name', 'phone', 'email', 'address_line1',
+                    'address_line2', 'address_line3', 'town', 'postcode',
+                    'country',)
+
+    def __init__(self, *args, **kwargs):
+
+        super().__init__(*args, **kwargs):
+            titles = {'name': 'Name',
+                        'phone': 'Phone Number (Include Area Code)',
+                        'email': 'Email Address',
+                        'address_line1': 'Address Line 1',
+                        'address_line2': 'Address Line 2',
+                        'address_line3': 'Address Line 3',
+                        'town': 'Town',
+                        'postcode': 'Eircode(Postcode)',
+                        'country': 'Country',
+                    }
--- purchases/views.py
+++ purchases/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from .forms import PurchaseForm
 # Create your views here.
-def order_payment(request):
 
+
+def order_payment(request):
 
     cart = request.session.get('cart', {})
     if not cart:
         messages.error(request, "Your cart is currently empty! Please see our gift collection to see our exciting range!")
         return redirect(reverse('gifts'))
     return render(request, 'purchases.html')
+
+    purchase_form = PurchaseForm()
+    template = 'purchases/purchases.html'
+    context = {
+        'purchase_form': purchase_form,
+    }
+
+    return render(request, template, context)
