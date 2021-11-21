979c0906f89d86cb942591062ff2dcbd02c1c2f7
shane-os <shaneosullivansos@yahoo.co.uk>
2021-11-21 02:13:54 +0000

    Update purchases urls

--- purchases/urls.py
+++ purchases/urls.py
@@ -1,8 +1,11 @@
 """ Purchases app url path """
 from django.urls import path
 from . import views
+from .webhooks import webhook
+
 
 urlpatterns = [
     path('', views.order_payment, name='purchases'),
     path('purchases_success/<order_number>', views.purchases_success, name='purchases_success'),
+    path('wh/', webhook, name='webhook'),
 ]
