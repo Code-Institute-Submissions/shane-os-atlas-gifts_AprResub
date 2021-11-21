2022fff7e8d538c7ccb0863a4ee7ffe9f4794bb0
shane-os <shaneosullivansos@yahoo.co.uk>
2021-11-21 01:52:51 +0000

    Update webhook event handling

--- purchases/webhooks.py
+++ purchases/webhooks.py
@@ -1,13 +1,39 @@
+from django.conf import settings
 from django.http import HttpResponse
+from purchases.webhook_handler import StripeWebhookHandler
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
 
-class StripeWebhookHandler:
-    """ Stripe Webhooks"""
-    def __init__(self, request):
-        self.request = request
+import stripe
 
-    def event_handle(self, event):
-        """ Unexpected event handling """
-        return HttpResponse(
-            response = f'Stripe Webhook Received: {event["type"]}',
-            status=200
+@require_POST
+@csrf_exempt
+def webhook(request):
+    """ Receive Stripe Webhooks """
+    wh_secret = settings.STRIPE_WH_SECRET
+    stripe.api_key = settings.STRIPE_SECRET_KEY
+    payload = request.body
+    sig_header = request.META['HTTP_STRIPE_SIGNATURE']
+    event = None
+
+    try:
+        event = stripe.Webhook.construct_event(
+            payload, sig_header, wh_secret
         )
+    except ValueError as e:
+        return HttpResponse(status=400)
+    except stripe.error.SignatureVerificationError as e:
+        return HttpResponse(status=400)
+    except Exception as e:
+        return HttpResponse(response=e, status=400)
+    
+    if event.type == 'payment_intent.succeeded':
+        payment_intent = event.data.object
+        print("Payment Intent was Successful!")
+    elif event.type == 'payment_method.attached':
+        payment_method = event.data.object
+        print("Payment Method was attached to a Customer!")
+    else:
+        return HttpResponse(status=400)
+    
+    return HttpResponse(status=200)
---
+++ purchases/webhooks_handler.py
@@ -0,0 +1,27 @@
+from django.http import HttpResponse
+
+class StripeWebhookHandler:
+    """ Stripe Webhooks"""
+    def __init__(self, request):
+        self.request = request
+
+    def handle_event(self, event):
+        """ Unexpected event handling """
+        return HttpResponse(
+            response = f'Stripe Webhook Received: {event["type"]}',
+            status=200
+        )
+
+    def handle_payment_intent_succeeded(self, event):
+        """ Stripe payment_intent_succeeded webhook handling """
+        return HttpResponse(
+            response = f'Stripe Unhandled Webhook Received: {event["type"]}',
+            status=200
+        )
+    
+    def handle_payment_intent_payment_failed(self, event):
+        """ Stripe payment_intent_payment_failed webhook handling  """
+        return HttpResponse(
+            response = f'Stripe Webhook Received: {event["type"]}',
+            status=200
+        )
