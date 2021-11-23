9569013509fe77ef47f6c47a4298ef3bf29b9e40
shane-os <shaneosullivansos@yahoo.co.uk>
2021-11-23 22:14:42 +0000

    Fix static files

---
+++ staticfiles/purchases/js/stripe.js
@@ -0,0 +1,60 @@
+// Stripe Payments
+var stripe_public_key = $('#public_key_id').text().slice(1,-1);
+var stripe_secret_key = $('#secret_key_id').text().slice(1,-1);
+var stripe = Stripe(stripe_public_key);
+var elements = stripe.elements();
+var style  = {
+    base: {
+        color: '#000',
+    },
+    invalid: {
+        color: '#cc0000',
+        iconColor: '#cc0000'
+    }
+};
+
+var stripe_card = elements.create('card', {style: style});
+stripe_card.mount('#card-payment');
+
+// Error Handling
+
+stripe_card.addEventListener('change', function (event) {
+    var errorResponse = document.getElementById('card-error');
+    if (event.error) {
+        var html = `
+            <span>$(event.error.message)</span>
+        `;
+        $(errorResponse).html(html);
+    } else {
+        errorResponse.textContent = '';
+    }
+});
+
+// Stripe Form Submission
+
+const stripe_form = document.getElementById('delivery-form');
+
+stripe_form.addEventListener('submit', function(e){
+    e.preventDefault();
+    card.update({ 'disabled': true});
+    $('#stripe-submit').attr('disabled', true);
+    stripe.confirmCardPayment(stripe_secret_key, {
+        payment_method: {
+            card: card,
+        }
+    }).then(function(result){
+        var errorResponse = document.getElementById('card-error');
+        if (result.error) {
+            var html = `
+            <span>$(event.error.message)</span>
+        `;
+        $(errorResponse).html(html);
+        card.update({ 'disabled': false});
+        $('#stripe-submit').attr('disabled', false);
+        } else {
+            if (result.payment_intent === 'succeeded'){
+                stripe_form.submit();
+            }
+        }
+    });
+});
