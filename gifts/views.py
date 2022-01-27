c81b0439de262b5dc3672d324ec2f58d9da24642
shane-os <shaneosullivansos@yahoo.co.uk>
2022-01-27 21:32:19 +0000

    Enable sort by price functionality

--- gifts/views.py
+++ gifts/views.py
@@ -5,7 +5,28 @@ from .models import Gift
 def gifts_list_all(request):
     """ Show gifts on page """
     gift = Gift.objects.all()
+    sort = None
+    direction = None
+
+    if request.GET:
+        if 'sort' in request.GET:
+            sortoption = request.GET['sort']
+            sort = sortoption
+
+            if sortoption == 'name':
+                sortoption = 'lower_name'
+                gift = gift.annotate(lower_name=Lower('name'))
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortoption = f'-{sortoption}'
+            gift = gift.order_by(sortoption)
+
+    sort_choice = f'{sort}_{direction}'
+
     context = {
-        "gifts": gift,
+            "gifts": gift,
+            "sort_choice": sort_choice,
     }
     return render(request, 'gifts/gifts.html', context)
