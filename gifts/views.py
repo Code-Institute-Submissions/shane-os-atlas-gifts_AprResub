7374c4935241816bf2b11e94d2a201fd740eca37
shane-os <shaneosullivansos@yahoo.co.uk>
2022-01-29 11:45:45 +0000

    Add search function to search box

--- gifts/views.py
+++ gifts/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.db.models import Q
 from .models import Gift
 
 
 def gifts_list_all(request):
     """ Show gifts on page """
     gift = Gift.objects.all()
+    searchquery = None
     sort = None
     direction = None
 
@@ -15,7 +18,7 @@ def gifts_list_all(request):
 
             if sortoption == 'name':
                 sortoption = 'lower_name'
-                gift = gift.annotate(lower_name=Lower('name'))
+                gift = gift.annotate(lower_name = Lower('name'))
 
             if 'direction' in request.GET:
                 direction = request.GET['direction']
@@ -23,10 +26,17 @@ def gifts_list_all(request):
                     sortoption = f'-{sortoption}'
             gift = gift.order_by(sortoption)
 
+            if 'q' in request.GET:
+                searchquery = request.GET['q']
+                searchqueries = Q(name__icontains=searchquery) | Q(description__icontains=searchquery)
+                gift = gift.filter(searchqueries)
+
     sort_choice = f'{sort}_{direction}'
 
     context = {
             "gifts": gift,
             "sort_choice": sort_choice,
+            "searchresult": searchquery,
     }
+    
     return render(request, 'gifts/gifts.html', context)
