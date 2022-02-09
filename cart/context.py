406c29ee2ca9cb9d93aa4ad92738b6539ab7ab1f
shane-os <shaneosullivansos@yahoo.co.uk>
2022-02-09 19:23:52 +0000

    Fix total value referencing error

--- cart/context.py
+++ cart/context.py
@@ -8,12 +8,12 @@ def cart_items(request):
     """ Gifts saved in cart """
     select_gifts = []
     gift_count = 0
-    total = 0
+    subtotal = 0
     cart = request.session.get('cart', {})
 
     for gift_id, quantity in cart.items():
         gift = get_object_or_404(Gift, pk=gift_id)
-        total += gift.price * quantity
+        subtotal += gift.price * quantity
         gift_count += quantity
         select_gifts.append({
             'gift_id': gift_id,
@@ -21,20 +21,22 @@ def cart_items(request):
             'gift': gift,
         })
 
-    if total > settings.DISCOUNT_THRESHOLD:
+    if subtotal > settings.DISCOUNT_THRESHOLD:
         discount_rate = Decimal(settings.DISCOUNT_RATE/100)
-        discount_amount = total * discount_rate
-        subtotal = total - discount_amount
-        subtotal = round(subtotal, 2)
-        total = subtotal + settings.DELIVERY_CHARGE
+        discount_amount = subtotal * discount_rate
+        total = subtotal - discount_amount
+        total = round(total, 2)
+        grandtotal = total + settings.DELIVERY_CHARGE
     else:
-        total = total + settings.DELIVERY_CHARGE
+        total = subtotal
+        grandtotal = total + settings.DELIVERY_CHARGE
 
     context = {
         'select_gifts': select_gifts,
         'gift_count': gift_count,
         'subtotal': subtotal,
         'total': total,
+        'grandtotal': grandtotal,
         'discount_threshold': settings.DISCOUNT_THRESHOLD,
         'discount_rate': settings.DISCOUNT_RATE,
         'delivery': settings.DELIVERY_CHARGE,
