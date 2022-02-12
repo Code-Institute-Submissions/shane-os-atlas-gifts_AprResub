2c95bff526884c450727aa0de2f4be7fdbc19b5f
shane-os <shaneosullivansos@yahoo.co.uk>
2022-02-12 18:41:13 +0000

    Create Home class and migrate

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,16 @@
+""" Home Page Text Administration """
 from django.contrib import admin
-
+from .models import Home
 # Register your models here.
+
+class HomeAdmin (admin.ModelAdmin):
+    """ Home Model Administration """
+    list_display = (
+        'title',
+        'main_text',
+    )
+
+    ordering = ('title',)
+
+
+admin.site.register(Home, HomeAdmin)
--- home/models.py
+++ home/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Home(models.Model):
+
+    title = models.CharField(max_length=200)
+    main_text = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.title
