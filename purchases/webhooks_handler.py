b3d787cc70aab8e3ecc40d5a97cb22bf46086849
shane-os <shaneosullivansos@yahoo.co.uk>
2022-02-26 14:20:34 +0000

    Fix typos in webhook handler file

--- purchases/webhooks_handler.py
+++ purchases/webhooks_handler.py
@@ -1,4 +1,6 @@
+""" Stripe Payments Webhook Handler """
 from django.http import HttpResponse
+
 
 class StripeWebhookHandler:
     """ Stripe Webhooks"""
@@ -8,20 +10,20 @@ class StripeWebhookHandler:
     def handle_event(self, event):
         """ Unexpected event handling """
         return HttpResponse(
-            response = f'Stripe Webhook Received: {event["type"]}',
+            response=f'Stripe Webhook Received: {event["type"]}',
             status=200
         )
 
     def handle_payment_intent_succeeded(self, event):
         """ Stripe payment_intent_succeeded webhook handling """
         return HttpResponse(
-            response = f'Stripe Unhandled Webhook Received: {event["type"]}',
+            response=f'Stripe Unhandled Webhook Received: {event["type"]}',
             status=200
         )
-    
+
     def handle_payment_intent_payment_failed(self, event):
         """ Stripe payment_intent_payment_failed webhook handling  """
         return HttpResponse(
-            response = f'Stripe Webhook Received: {event["type"]}',
+            response=f'Stripe Webhook Received: {event["type"]}',
             status=200
         )
