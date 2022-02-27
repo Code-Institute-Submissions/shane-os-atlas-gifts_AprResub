81bd6135f11e3aa2f8e6a0cbbdbb96821dcc900e
shane-os <shaneosullivansos@yahoo.co.uk>
2022-02-27 18:53:03 +0000

    Apply PEP8 Python code style guide to files

--- purchases/admin.py
+++ purchases/admin.py
@@ -2,6 +2,7 @@
 from django.contrib import admin
 from .models import Purchase, LineItem
 
+
 class LineItemAdminInLine(admin.TabularInline):
     """ Line items administration """
     model = LineItem
@@ -9,15 +10,18 @@ class LineItemAdminInLine(admin.TabularInline):
 
 
 class PurchaseAdmin(admin.ModelAdmin):
+    """ Site Purchases Admin """
     inlines = (LineItemAdminInLine,)
     """ Purchases form administration """
-    readonly_fields = ('order_number', 'date', 'pre_discount_total', 'discount', 'final_total')
+    readonly_fields = ('order_number', 'date', 'pre_discount_total',
+                       'discount', 'final_total')
 
     fields = ('name', 'phone', 'email', 'address_line1',
               'address_line2', 'address_line3', 'town', 'postcode',
               'country', 'order_number', 'date', 'pre_discount_total',)
 
-    list_display = ('order_number', 'date', 'name', 'email', 'pre_discount_total',)
+    list_display = ('order_number', 'date', 'name', 'email',
+                    'pre_discount_total',)
 
     ordering = ('-date',)
 
