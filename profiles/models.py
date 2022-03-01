5fc71480562a599defda97baedbffc0d58592059
shane-os <shaneosullivansos@yahoo.co.uk>
2022-03-01 08:12:50 +0000

    Apply PEP8 styling to profiles model

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,4 @@
+""" Create/ Update User Profile """
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
@@ -5,11 +6,15 @@ from django.dispatch import receiver
 
 
 class UserAccount(models.Model):
+    """ User Profile Saved Details """
     profile = models.OneToOneField(User, on_delete=models.CASCADE)
     official_phone = models.CharField(max_length=15, null=True, blank=True)
-    official_address_line1 = models.CharField(max_length=100, null=True, blank=True)
-    official_address_line2 = models.CharField(max_length=100, null=True, blank=True)
-    official_address_line3 = models.CharField(max_length=100, null=True, blank=True)
+    official_address_line1 = models.CharField(max_length=100,
+                                              null=True, blank=True)
+    official_address_line2 = models.CharField(max_length=100,
+                                              null=True, blank=True)
+    official_address_line3 = models.CharField(max_length=100,
+                                              null=True, blank=True)
     official_town = models.CharField(max_length=100, null=True, blank=True)
     official_postcode = models.CharField(max_length=20, null=True, blank=True)
     official_country = models.CharField(max_length=20, null=True, blank=True)
@@ -18,8 +23,10 @@ class UserAccount(models.Model):
         return self.profile
 
 
+"""
 @receiver(post_save, sender=User)
 def new_update_profile_info(sender, instance, new, **kwargs):
     if new:
         UserAccount.objects.create(profile=instance)
     instance.UserAccount.save()
+"""
