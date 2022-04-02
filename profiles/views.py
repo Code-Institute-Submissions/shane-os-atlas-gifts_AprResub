e8e91141db034202fe6da426af6f681cc08aea8e
shane-os <shaneosullivansos@yahoo.co.uk>
2022-04-02 16:45:15 +0000

    Enable profile info form saving

--- profiles/views.py
+++ profiles/views.py
@@ -1,13 +1,21 @@
 """ User Account """
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 from .models import UserAccount
 from .forms import UserAccountForm
 
 
 def profile(request):
     """ Show User Account """
-    print(request)
+    
     profile = get_object_or_404(UserAccount, user=request.user)
+
+    if request.method == "POST":
+        profile_update = UserAccountForm(request.POST, instance=profile)
+        if profile_update.is_valid():
+            profile_update.save()
+            messages.success(request, "Your profile was saved successfully!")
+    
     form = UserAccountForm(instance=profile)
     template = 'profiles/profile.html'
     context = {
