e07b4a229bdc30a5f874918a1be9e0f6368f62db
shane-os <shaneosullivansos@yahoo.co.uk>
2022-04-02 10:05:39 +0000

    Add personal info form to profile

--- profiles/views.py
+++ profiles/views.py
@@ -8,12 +8,11 @@ def profile(request):
     """ Show User Account """
     print(request)
     profile = get_object_or_404(UserAccount, user=request.user)
-    '''
-    form = UserAccountForm(instance=user)
-    '''
+    form = UserAccountForm(instance=profile)
     template = 'profiles/profile.html'
     context = {
         'profile': profile,
+        'form': form,
     }
 
     return render(request, template, context)
