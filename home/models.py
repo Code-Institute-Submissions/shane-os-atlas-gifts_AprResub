0359153de6c99abcdf5319b6ff919652ebff6716
shane-os <shaneosullivansos@yahoo.co.uk>
2022-04-18 18:10:04 +0000

    Update home page app model

--- home/models.py
+++ home/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Home(models.Model):
 
     title = models.CharField(max_length=200)
-    main_text = models.CharField(max_length=1000)
+    main_text = models.TextField(max_length=1000)
 
     def __str__(self):
         return self.title
