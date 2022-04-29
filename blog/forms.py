b7a80ab620fdc3b817da80726fa26f1399117bda
shane-os <shaneosullivansos@yahoo.co.uk>
2022-04-29 02:11:39 +0000

    Fix error in blog forms

--- blog/forms.py
+++ blog/forms.py
@@ -5,5 +5,7 @@ from .models import Post
 
 class BlogForm(forms.ModelForm):
     """ Form to create blog posts"""
-    model = Post
-    fields = ('title', 'body', 'created_by', 'image_url', 'image')
+    class Meta:
+
+        model = Post
+        fields = ('title', 'body', 'created_by', 'image_url', 'image')
